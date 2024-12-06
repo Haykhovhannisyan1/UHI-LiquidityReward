72525035461fe9a9e81bd877787ac135dea7025e
Haykhovhannisyan1 <hayk.9816@gmail.com>
2024-12-06 17:50:25 +0400

    add LiquidityReward circuit

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,8 +1,49 @@
-import { Brevis, ErrCode, ProofRequest, Prover, ReceiptData, Field } from 'brevis-sdk-typescript';
 import { ethers } from 'ethers';
+import { Brevis, ErrCode, ProofRequest, Prover, TransactionData, ReceiptData, Field, StorageData } from 'brevis-sdk-typescript';
+import Web3, { Contract, ContractAbi, EventLog } from 'web3';
+import { readFileSync } from 'fs';
+import { RegisteredSubscription } from 'web3/lib/commonjs/eth.exports';
 
+
+async function getStorageDataAtBlock(chosenBlock: number, contractAddress: string, index: number, web3: Web3<RegisteredSubscription>) {
+    const storageVal = await web3.eth.getStorageAt(contractAddress, index, chosenBlock);
+
+    const storageData = new StorageData({
+        block_num: chosenBlock,
+        address: contractAddress,
+        slot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+        value: storageVal
+    })
+    console.log(chosenBlock, storageVal)
+
+    return storageData
+}
+
+
+
+// Get N storage values
+async function getStorageDataPoints(numDataPoints: number, startingBlock: number, contractAddress: string, index: number, web3: Web3<RegisteredSubscription>) {
+    var dataPoints = []
+
+    var curBlock = startingBlock;
+
+    // repeat until we meet the requested data points
+    while (dataPoints.length < numDataPoints) {
+        // fetch brevis structured data
+        const storageItem = await getStorageDataAtBlock(curBlock, contractAddress, index, web3);
+        // append new item
+        dataPoints.push(storageItem)
+        // move to next block
+        curBlock--;
+    }
+
+    return dataPoints;
+}
 async function main() {
+    const web3 = new Web3('https://eth.llamarpc.com');
     const prover = new Prover('localhost:33247');
+    // const prover = new Prover('127.0.0.1:8545');
+    // const brevis = new Brevis('appsdk.brevis.network:11080');
     const brevis = new Brevis('appsdkv3.brevis.network:443');
 
     const proofReq = new ProofRequest();
@@ -17,28 +58,34 @@ async function main() {
 
     if (hash.length === 0) {
         console.error("empty transaction hash")
-        return 
+        return
     }
-    
-    proofReq.addReceipt(
-        new ReceiptData({
-            tx_hash: hash,
-            fields: [
-                new Field({
-                    log_pos: 0,
-                    is_topic: true,
-                    field_index: 1,
-                }),
-                new Field({
-                    log_pos: 0,
-                    is_topic: false,
-                    field_index: 0,
-                }),
-            ],
-        }),
-    );
+
+    // proofReq.addReceipt(
+    //     new ReceiptData({
+    //         tx_hash: hash,
+    //         fields: [
+    //             new Field({
+    //                 log_pos: 0,
+    //                 is_topic: true,
+    //                 field_index: 1,
+    //             }),
+    //             new Field({
+    //                 log_pos: 0,
+    //                 is_topic: false,
+    //                 field_index: 0,
+    //             }),
+    //         ],
+    //     }),
+    // );
 
     console.log(`Send prove request for ${hash}`)
+
+    const storageDataList = await getStorageDataPoints(500, 19308799, "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640", 0, web3)
+
+    for (let i = 0; i < storageDataList.length; i++) {
+        proofReq.addStorage(storageDataList[i], i)
+    }
 
     const proofRes = await prover.prove(proofReq);
     // error handling
--- prover/circuits/circuit.go
+++ prover/circuits/circuit.go
@@ -2,45 +2,53 @@ package circuits
 
 import (
 	"github.com/brevis-network/brevis-sdk/sdk"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 type AppCircuit struct{}
 
 var USDCTokenAddr = sdk.ConstUint248("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48")
-var minimumVolume = sdk.ConstUint248(500000000) // minimum 500 USDC
+var PositionManager = sdk.ConstUint248("0x1b1c77b606d13b09c84d1c7394b96b147bc03147")
+var EventIdLiquidity = sdk.ParseEventID(
+	hexutil.MustDecode("0xf208f4912782fd25c7f114ca3723a2d5dd6f3bcc3ac8db5af63baa85f711d5ec"))
+var EventIdTransfer = sdk.ParseEventID(
+		hexutil.MustDecode("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"))
+var minimumLiqudity = sdk.ConstUint248(5000000000) // minimum 5000 USDC
 var _ sdk.AppCircuit = &AppCircuit{}
 
 func (c *AppCircuit) Allocate() (maxReceipts, maxStorage, maxTransactions int) {
 	// Our app is only ever going to use one storage data at a time so
 	// we can simply limit the max number of data for storage to 1 and
 	// 0 for all others
-	return 32, 0, 0
+	return 64, 0, 0
 }
 
 func (c *AppCircuit) Define(api *sdk.CircuitAPI, in sdk.DataInput) error {
 	receipts := sdk.NewDataStream(api, in.Receipts)
-	receipt := sdk.GetUnderlying(receipts, 0)
+	receipt0 := sdk.GetUnderlying(receipts, 0)
+	receipt1 := sdk.GetUnderlying(receipts, 1)
 
-	// Check logic
-	// The first field exports `from` parameter from Transfer Event
-	// It should use the second topic in Transfer Event log
-	api.Uint248.AssertIsEqual(receipt.Fields[0].Contract, USDCTokenAddr)
-	api.Uint248.AssertIsEqual(receipt.Fields[0].IsTopic, sdk.ConstUint248(1))
-	api.Uint248.AssertIsEqual(receipt.Fields[0].Index, sdk.ConstUint248(1))
-
-	// Make sure two fields uses the same log to make sure account address linking with correct volume
-	api.Uint32.AssertIsEqual(receipt.Fields[0].LogPos, receipt.Fields[1].LogPos)
-
-	// The second field exports `Volume` parameter from Transfer Event
-	// It should use Data in Transfer Event log
-	api.Uint248.AssertIsEqual(receipt.Fields[1].IsTopic, sdk.ConstUint248(0))
-	api.Uint248.AssertIsEqual(receipt.Fields[1].Index, sdk.ConstUint248(0))
-
+	api.Uint248.AssertIsEqual(receipt0.Fields[0].Contract, PositionManager)
+	api.Uint248.AssertIsEqual(receipt0.Fields[0].IsTopic, sdk.ConstUint248(0))
+	api.Uint248.AssertIsEqual(receipt0.Fields[0].Index, sdk.ConstUint248(2))
+	api.Uint248.AssertIsEqual(receipt0.Fields[0].EventID, EventIdLiquidity)
 	// Make sure this transfer has minimum 500 USDC volume
-	api.Uint248.AssertIsLessOrEqual(minimumVolume, api.ToUint248(receipt.Fields[1].Value))
+	api.Uint248.AssertIsLessOrEqual(minimumLiqudity, api.ToUint248(receipt0.Fields[1].Value))
 
-	api.OutputUint(64, api.ToUint248(receipt.BlockNum))
-	api.OutputAddress(api.ToUint248(receipt.Fields[0].Value))
-	api.OutputBytes32(receipt.Fields[1].Value)
+
+	api.Uint248.AssertIsEqual(receipt1.Fields[0].Contract, USDCTokenAddr)
+	api.Uint248.AssertIsEqual(receipt1.Fields[0].IsTopic, sdk.ConstUint248(1))
+	api.Uint248.AssertIsEqual(receipt1.Fields[0].Index, sdk.ConstUint248(1))
+	api.Uint248.AssertIsEqual(receipt1.Fields[0].EventID, EventIdTransfer)
+	// Make sure two fields uses the same log to make sure account address linking with correct volume
+	api.Uint32.AssertIsEqual(receipt1.Fields[0].LogPos, receipt1.Fields[1].LogPos)
+
+	api.Uint248.AssertIsEqual(receipt1.Fields[1].IsTopic, sdk.ConstUint248(0))
+	api.Uint248.AssertIsEqual(receipt1.Fields[1].Index, sdk.ConstUint248(0))
+	api.Bytes32.AssertIsEqual(receipt0.Fields[0].Value, receipt1.Fields[1].Value)//TODO
+
+	api.OutputUint(64, api.ToUint248(receipt0.BlockNum))
+	api.OutputAddress(api.ToUint248(receipt1.Fields[0].Value))
+	api.OutputBytes32(receipt0.Fields[1].Value)
 	return nil
 }
